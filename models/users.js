c3a68ab73e3d7bdf72a0c04508c5e02e9279a8f7
Mozeeb Abdulha <mozeebabdulha@gmail.com>
2020-01-30 20:25:50 +0000

    Added model

---
+++ models/users.js
@@ -0,0 +1,48 @@
+let mongoose = require('mongoose');
+
+let UserSchema = new mongoose.Schema({
+    fName: {
+        type: String,
+        required: true
+    },
+    lName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // Regex to validate email address
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+},
+    {
+        collection: 'users'
+});
+
+mongoose.set('useCreateIndex', true);
+
+module.exports = mongoose.model('User', UserSchema);
