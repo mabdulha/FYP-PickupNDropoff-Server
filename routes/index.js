2559231c531d69c35161a1a7840d01defd8d9080
Mozeeb Abdulha <mozeebabdulha@gmail.com>
2020-01-30 20:04:34 +0000

    connected project with mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+let mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config()
+
+let mongodbUri = process.env.MONGO_URI
+
+mongoose.connect(mongodbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+mongoose.set("useFindAndModify", false)
+
+let db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log('Unable to Connect to [ ' + db.name + ' ]', err);
+});
+
+db.once('open', function () {
+    console.log('Successfully Connected to [ ' + db.name + ' ]');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pickup and Dropoff' });
 });
 
 module.exports = router;
