021cf63ecce064971b6829f138ee99595647d264
Mozeeb Abdulha <mozeebabdulha@gmail.com>
2020-02-08 23:16:35 +0000

    Post register user endpoint tested

--- test/functional/api/usersTest.js
+++ test/functional/api/usersTest.js
@@ -1,9 +1,31 @@
-const chai = require('chai')
-const expect = chai.expect
-const request = require('supertest')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
-const User = require('../../../models/users')
+const chai = require("chai");
+const server = require("../../../bin/www");
+const expect = chai.expect;
+const request = require("supertest");
+const _ = require("lodash");
 
-const _ = require('lodash')
+let datastore = require("../../../models/users");
+
+describe('Userss',  () => {
+    describe('POST /users/', () => {
+      it('should register user to the database', () => {
+        const user = {
+          fname: 'Mozeeb',
+          lname: 'Abdulha',
+          username: 'moz',
+          email: 'moz@gmail.com',
+          password: 'secret',
+          phone: '0894536791',
+          address: 'Kilkenny',
+          avatar: 'https://firebasestorage.googleapis.com/v0/b/pickupndropoff-fab91.appspot.com/o/avatars%2Fdefault-profile.png?alt=media&token=c0d9df92-b5f6-4526-bfc2-2a27d91519e0'
+        }
+        return request(server)
+          .post('/api/users/register')
+          .send(user)
+          .expect(200)
+          .then(res => {
+            expect(res.body.message).equal('User registered successfully')
+          })
+      })
+    })
+});
